f6fc1dbf271ca263a5e574d4ed28ac0844bcbbac
yikejason <18215513386@163.com>
2024-12-14 16:22:38 +0800

    feature: create a notify server skeleton

--- notify_server/src/lib.rs
+++ notify_server/src/lib.rs
@@ -1,12 +1,15 @@
 mod sse;
 
+use anyhow::Result;
 use axum::{
     response::{Html, IntoResponse},
     routing::get,
     Router,
 };
-
+use futures::StreamExt;
+use sqlx::postgres::PgListener;
 use sse::*;
+use tracing::info;
 
 const INDEX_HTML: &str = include_str!("../index.html");
 
@@ -14,6 +17,24 @@ pub fn get_router() -> Router {
     Router::new()
         .route("/", get(index_handler))
         .route("/events", get(sse_handler))
+}
+
+// search sqlx pglistener docs to write it
+pub async fn set_up_pg_listener() -> Result<()> {
+    let mut listener =
+        PgListener::connect("postgresql://postgres:postgres@localhost:5432/chat").await?;
+    listener.listen("chat_updated").await?;
+    listener.listen("chat_message_created").await?;
+
+    let mut stream = listener.into_stream();
+
+    tokio::spawn(async move {
+        while let Some(Ok(notifi)) = stream.next().await {
+            info!("Received notification: {:?}", notifi);
+        }
+    });
+
+    Ok(())
 }
 
 pub(crate) async fn index_handler() -> impl IntoResponse {
--- notify_server/src/main.rs
+++ notify_server/src/main.rs
@@ -1,17 +1,19 @@
 use std::net::SocketAddr;
 
 use anyhow::Result;
-use notify_server::get_router;
+use notify_server::{get_router, set_up_pg_listener};
 use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, Layer};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let layer = fmt::layer().pretty().with_filter(LevelFilter::INFO);
+    let layer = fmt::layer().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 6687));
+
+    set_up_pg_listener().await?;
 
     let listener = TcpListener::bind(&addr).await?;
     info!("Listening on: {}", addr);
